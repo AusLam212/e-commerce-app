f3d63c1bbe7a5423c3f90bbe0f70c0bb942e70f5
Austin Lambert <atlambert11@gmail.com>
2021-05-03 19:14:38 -0500

    Updated menu item to navigate withRouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import './App.css';
 import Homepage from "./pages/Homepage/Homepage"
+import Hats from "./pages/Hats/Hats";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={Hats} />
       </Switch>
     </div>
   );
--- crwn-clothing/src/components/MenuItem/MenuItem.js
+++ crwn-clothing/src/components/MenuItem/MenuItem.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./MenuItem.scss";
+import { withRouter } from "react-router-dom";
 
-function MenuItem(props) {
+function MenuItem({ title, imageUrl, size, history, linkUrl, match }) {
     return (
-        <div className={`${props.size} menu-item`}>
-            <div className="background-image" style={{backgroundImage: `url(${props.img})`}}/>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
             <div className="content">
-                <h1 className="title">{props.title}</h1>
+                <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
