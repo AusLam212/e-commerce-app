5b9a797cc50a4f0c89bde3395715ca3e12e9c333
Austin Lambert <atlambert11@gmail.com>
2021-05-10 13:04:57 -0500

    Fixed pathing on hats router

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={Hats} />
+        <Route path="/hats" component={Hats} />
       </Switch>
     </div>
   );
