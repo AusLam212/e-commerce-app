d219c105c3370eb82dcab778040b42ec85b0539e
Austin Lambert <atlambert11@gmail.com>
2021-05-13 13:54:20 -0500

    Fixed user sign in and sign out with firebase and added google button styling

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+
 import './App.css';
+
 import Homepage from "./pages/Homepage/Homepage"
 import ShopPage from "./pages/Shop/ShopPage";
 import Header from "./components/Header/Header";
 import SignInSignUp from "./pages/SignInSignUp/SignInSignUp";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currenUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currenUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currenUser} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- crwn-clothing/src/components/Header/Header.js
+++ crwn-clothing/src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import "./Header.scss";
 
 import { ReactComponent as Logo} from "../../assets/crown.svg"
 
-function Header() {
+function Header({ currentUser }) {
     return (
         <div className="header">
             <Link className="logo-container" to="/">
@@ -18,6 +20,14 @@ function Header() {
                 <Link className="option" to="/shop">
                     CONTACT
                 </Link>
+                {
+                    currentUser ?
+                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className="option" to="/signin">
+                        SIGN IN
+                    </Link>
+                }
             </div>
         </div>
     )
